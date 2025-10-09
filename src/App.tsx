6b6f14d95f12f4753669b8467eecb85911ffd2da
Nikkihosseini <niekal00@gmail.com>
2025-10-09 19:56:05 +0330

    refactor: convert ToDoApp component to TypeScrip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import './index.css'
+import ToDoApp from './Pages/ToDoApp';
+import { store } from './component/store/store';
+import { Provider } from 'react-redux';
+import './index.css';
 
-
-export default function App() {
-
-
+function App() {
   return (
     <>
-     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-900">
-        Tailwind v3 + React + TypeScript ✨
-      </h1>
-    </div>
+      <Provider store={store}>
+        <ToDoApp />
+      </Provider>
     </>
-  )
+  );
 }
 
-
+export default App;
